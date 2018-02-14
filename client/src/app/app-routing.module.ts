374fb0b3ffc159d8f2ef3118aa2e62efe4158a5e
Jean Bosse <bosse_jean@outlook.com>
2018-02-13 23:01:03 -0500

    added some files

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [],
+  bootstrap: []
+})
+
+export class AppRoutingModule { }
